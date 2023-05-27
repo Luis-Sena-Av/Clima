3ec1827cb2f2ef3a024ebf79c7b51b1f11928fe3
Luis-Sena-Av <luchito721@hotmail.com>
2023-05-27 18:06:16 -0500

    primera

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import { DatosClima } from './Componentes/DatosClima'
 function App() {
   const [coords, setcoords] = useState()
   const [clima, setclima] = useState()
-
+  const [mostrar, setmostrar] = useState()
   
 
   useEffect(()=>{
+    
     const success= pos=>{
       const coordenadas={lat:pos.coords.latitude, lon:pos.coords.longitude }
       setcoords(coordenadas)
+      setmostrar("true")
     }
 
     const error=err=> {
       console.warn(`ERROR(${err.code}): ${err.message}`);
+      setmostrar("false")
     }
     navigator.geolocation.getCurrentPosition(success,error);
 
   },[])
 
-  
+
 
   useEffect(()=>{
     if(coords){
@@ -37,13 +40,16 @@ function App() {
   // const estilo={
   //   backgroundImage:'public\imagenes\despejado.jpg'
   // }
+  
+
 
   return (
     
     
     <div className='targeta' >
-      <DatosClima clima={clima}/>
-     
+
+      {clima? <DatosClima clima={clima}/> : <div className='loading'> <div className='load'></div><h1 className='text'>Loading</h1></div>  }
+       
     </div>
       
     
--- src/Componentes/DatosClima.jsx
+++ src/Componentes/DatosClima.jsx
@@ -7,11 +7,14 @@ export const DatosClima = ({clima}) => {
         setscale(!scale)
     }
    
+    
+
+
   return (
     <div className='targeta1'>
         <div className='name'>
-            <h2>Weather App</h2>
-            <h4>{clima?.name}, {clima?.sys.country}</h4>
+            <h2 className='title'>Weather App</h2>
+            <h4 className='subtitle'>{clima?.name}, {clima?.sys.country}</h4>
         </div>
 
         <div className='datos'>
@@ -19,7 +22,7 @@ export const DatosClima = ({clima}) => {
             <div > <img src={clima ?`https://openweathermap.org/img/wn/${clima.weather[0].icon}@2x.png`:""} alt="" /></div>
             <div className='datos1'>
             <b>"{clima?.weather[0].description}"</b>
-            <span>Wind Speed<b>  {clima?.wind.speed} m/s</b></span>
+            <span>Wind Speed<b>  {(clima?.wind.speed)*3.6} km/h</b></span>
             <span>Clouds<b>    {clima?.clouds.all}%</b></span>
             <span>Pressure<b>   {clima?.main.pressure} hPa</b></span>
             </div>
@@ -27,7 +30,7 @@ export const DatosClima = ({clima}) => {
         </div>
 
         <div className='temp'>
-            <b>{scale? <h3>{(clima?.main.temp- 273.15).toFixed(2)} °C</h3>: <h3>{((clima?.main.temp- 273.15)*1.8+32).toFixed(2)} °F</h3> }</b>
+            <b>{scale? <h3>{(clima?.main.temp- 273.15).toFixed(1)} °C</h3>: <h3>{((clima?.main.temp- 273.15)*1.8+32).toFixed(1)} °F</h3> }</b>
             <button onClick={handlescale}> {scale? <h3>Change to °F</h3>: <h3>Change to °C</h3> }</button>
         </div>
 
