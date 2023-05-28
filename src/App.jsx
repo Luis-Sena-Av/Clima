358f3433e5c0d030d4e4e000858b208933644216
Luis-Sena-Av <luchito721@hotmail.com>
2023-05-28 12:58:37 -0500

    otra primera

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { DatosClima } from './Componentes/DatosClima'
 function App() {
   const [coords, setcoords] = useState()
   const [clima, setclima] = useState()
-  const [mostrar, setmostrar] = useState()
+  const [mostrar, setmostrar] = useState("undefined")
   
 
   useEffect(()=>{
@@ -14,17 +14,18 @@ function App() {
     const success= pos=>{
       const coordenadas={lat:pos.coords.latitude, lon:pos.coords.longitude }
       setcoords(coordenadas)
-      setmostrar("true")
+     
     }
 
     const error=err=> {
       console.warn(`ERROR(${err.code}): ${err.message}`);
-      setmostrar("false")
+     
     }
     navigator.geolocation.getCurrentPosition(success,error);
 
   },[])
 
+  
 
 
   useEffect(()=>{
@@ -37,16 +38,62 @@ function App() {
     
   },[coords])
 
-  // const estilo={
-  //   backgroundImage:'public\imagenes\despejado.jpg'
-  // }
+  let expresión=Number(clima?.weather[0].id)
+  let fondo=""
+
+  switch (true) {
+
+    case expresión>=200 && expresión<300:
+      fondo='url(../imagenes/tormenta.jpg)'
+    break;
+      
+    case expresión>=300 && expresión<400:
+      fondo='url(../imagenes/llovizna.jpg)'
+    break;
+
+    case expresión>=500 && expresión<600:
+      fondo='url(../imagenes/lluvia.jpg)'
+    break;
+
+    case expresión>=600 && expresión<700:
+      fondo='url(../imagenes/nieve.jpg)'
+    break;
+
+    case expresión>=700 && expresión<800:
+      fondo='url(../imagenes/atmosfera.jpg)'
+    break;
+
+    case 800:
+      fondo='url(../imagenes/clear-sky.jpg)'
+    break;
+
+    case expresión>=801 && expresión<900:
+      fondo='url(../imagenes/Clouds.jpg)'
+    break;
+
+    default:
+       fondo='url(../imagenes/despejado.jpg)'
+    break;
+      
+  }
+
+
+
+  
+
+  const estilo={
+    backgroundImage:fondo,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height:'100vh'
+  }
   
 
 
   return (
     
     
-    <div className='targeta' >
+    <div className='targeta' style={estilo}>
 
       {clima? <DatosClima clima={clima}/> : <div className='loading'> <div className='load'></div><h1 className='text'>Loading</h1></div>  }
        
